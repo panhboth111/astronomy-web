569c76ffb876bbaf13f0efdcbb3c52751fdb5aa8
panhboth111 <neakpanhboth18@kit.edu.kh>
2020-03-12 13:30:44 +0700

    feat: 🎸 base setup and home page

---
+++ src/components/NavBar/NavBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app-bar app dark flat fixed clipped-right>
+    <v-toolbar-title class="display-1 font-weight-light"
+      ><span>ASTRO</span><span class="purple--text">KIT</span></v-toolbar-title
+    >
+
+    <v-spacer></v-spacer>
+    <div class="hidden-sm-and-down mr-10">
+      <router-link
+        v-for="(item, i) in items"
+        :key="i"
+        :to="item.router"
+        style="text-decoration:none"
+        class="black--text"
+      >
+        <v-btn class="subtitle font-weight-light">{{ item.title }}</v-btn>
+      </router-link>
+    </div>
+    <v-app-bar-nav-icon
+      @click="drawerToggle"
+      class="hidden-md-and-up"
+    ></v-app-bar-nav-icon>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    items: [
+      { title: "About", router: "/about" },
+      { title: "Documents", router: "/documents" },
+      { title: "Contact", router: "/contact" }
+    ]
+  })
+};
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,75 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <section>
+      <v-parallax :src="images.parallax" height="650">
+        <v-container>
+          <v-row column>
+            <v-col
+              class="text-md-center text-xl-center text-md-center text-lg-center"
+            >
+              <div class="display-2 font-weight-thin">
+                ASTRONOMY
+                <span class="white--text font-weight-light ">CLUB</span>
+              </div>
+              <div class="font-weight-light">
+                Somewhere, something incredible is waiting to be known
+              </div>
+              <div class="mt-5">
+                <v-btn outlined color="white">Learn More</v-btn>
+              </div>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-parallax>
+    </section>
+    <section>
+      <v-container>
+        <v-row>
+          <v-col
+            class="my-12"
+            v-for="(item, i) in items"
+            :key="i"
+            cols="12"
+            md="12"
+            sm="12"
+          >
+            <v-card dark>
+              <v-container>
+                <v-card-title class="display-1 font-weight-thin text-uppercase">
+                  {{ item.title }}
+                </v-card-title>
+                <div>
+                  <v-img :src="item.image" max-width="400"></v-img>
+                </div>
+              </v-container>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </section>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import parallax from "../assets/parallax5.jpg";
+import bio from "../assets/bio.jpg";
+import bh from "../assets/bh.jpg";
+import phy from "../assets/phy.jpg";
+import stars from "../assets/stars.jpg";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: {},
+  data: () => ({
+    images: {
+      parallax
+    },
+    items: [
+      { title: "Astro Biology", content: "", image: bio },
+      { title: "Astro Physic", content: "", image: phy },
+      { title: "Black hole", content: "", image: bh },
+      { title: "Stars", content: "", image: stars }
+    ]
+  })
+};
 </script>
