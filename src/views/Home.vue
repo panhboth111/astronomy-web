7cd9343413c1995bbda711c85ad7e452a81d8787
panhboth111 <neakpanhboth18@kit.edu.kh>
2020-03-14 11:22:26 +0700

    refactor: 💡 restyle home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -32,9 +32,6 @@
           </v-card-title>
           <v-row>
             <v-col sm="12" md="6" cols="12">
-              <v-img :src="images.pic3" max-width="600px"> </v-img>
-            </v-col>
-            <v-col sm="12" md="6" cols="12">
               <v-card-content class="white--text title font-weight-thin">
                 Astronomy is the study of the sun, moon, stars, planets, comets,
                 gas, galaxies, gas, dust and other non-Earthly bodies and
@@ -46,12 +43,48 @@
                 astronomy and related fields of study, including cosmology.
               </v-card-content>
             </v-col>
+            <v-col sm="12" md="6" cols="12">
+              <v-img :src="images.pic3" max-width="600px"> </v-img>
+            </v-col>
           </v-row>
         </v-container>
       </v-card>
     </section>
-    <section class="mt-10">
-      <v-parallax :src="images.test5" height="500" class="bg">
+    <section class="my-md-10">
+      <v-card color="transparent">
+        <v-container>
+          <v-card-title
+            class="headline font-weight-thin justify-center white--text text-uppercase"
+          >
+            Origin of Astronomy
+          </v-card-title>
+          <v-row>
+            <v-col sm="12" md="4" cols="12">
+              <v-img :src="images.planet2" max-width="600px"> </v-img>
+            </v-col>
+            <v-col sm="12" md="6" cols="12" class="mr-12">
+              <v-card-content class="white--text title font-weight-thin">
+                Astronomy is the oldest of the natural sciences, dating back to
+                antiquity, with its origins in the religious, mythological,
+                cosmological, calendrical, and astrological beliefs and
+                practices of prehistory: vestiges of these are still found in
+                astrology, a discipline long interwoven with public and
+                governmental astronomy. It was not completely separated in
+                Europe (see astrology and astronomy) during the Copernican
+                Revolution starting in 1543. In some cultures, astronomical data
+                was used for astrological prognostication. The study of
+                astronomy has received financial and social support from many
+                institutions, especially the Church, which was its largest
+                source of support between the 12th century to the
+                Enlightenment.[1]
+              </v-card-content>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card>
+    </section>
+    <section>
+      <v-parallax :src="images.test5" height="460" class="bg">
         <v-container>
           <v-row column>
             <v-col
@@ -68,6 +101,25 @@
         </v-container></v-parallax
       >
     </section>
+
+    <!-- <section class="mt-10">
+      <v-parallax :src="images.sun" height="500" class="bg">
+        <v-container>
+          <v-row column>
+            <v-col
+              class="text-md-center text-xl-center text-md-center text-lg-center"
+            >
+              <div class="font-weight-light">
+                There are many branches of astronomy
+              </div>
+              <div class="mt-5">
+                <v-btn color="blue" outlined>EXPLORE</v-btn>
+              </div>
+            </v-col>
+          </v-row>
+        </v-container></v-parallax
+      >
+    </section> -->
     <footer class="mt-12">
       <v-container>
         <v-row>
@@ -105,7 +157,7 @@ import parallax from "../assets/parallax5.jpg";
 import pic3 from "../assets/test.jpg";
 import test2 from "../assets/test2.jpg";
 import test5 from "../assets/test5.jpg";
-
+import planet2 from "../assets/planet2.jpg";
 export default {
   name: "Home",
   components: {},
@@ -114,9 +166,14 @@ export default {
       parallax,
       pic3,
       test2,
-      test5
+      test5,
+      planet2
     }
-  })
+  }),
+  created() {
+    const d = new Date();
+    console.log(d.getHours());
+  }
 };
 </script>
 <style scoped></style>
